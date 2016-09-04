587cc729a460b9979f97c319d7fcae01d4a02d14
RamAnanth <ram.ananth14@gmail.com>
2016-09-04 12:42:43 +0530

    Adding rest of pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,11 +8,41 @@
  *
  * Main module of the application.
  */
- $(document).ready(function(){
 
+
+// Function to validate date in proper format
+jQuery.validator.addMethod( "dateITA", function( value, element ) {
+  var check = false,
+    re = /^\d{1,2}\/\d{1,2}\/\d{4}$/,
+    adata, gg, mm, aaaa, xdata;
+  if ( re.test( value ) ) {
+    adata = value.split( "/" );
+    gg = parseInt( adata[ 0 ], 10 );
+    mm = parseInt( adata[ 1 ], 10 );
+    aaaa = parseInt( adata[ 2 ], 10 );
+    xdata = new Date( Date.UTC( aaaa, mm - 1, gg, 12, 0, 0, 0 ) );
+    if ( ( xdata.getUTCFullYear() === aaaa ) && ( xdata.getUTCMonth() === mm - 1 ) && ( xdata.getUTCDate() === gg ) ) {
+      check = true;
+    } else {
+      check = false;
+    }
+  } else {
+    check = false;
+  }
+  return this.optional( element ) || check;
+}, $.validator.messages.date );
+
+
+
+
+
+
+$(document).ready(function(){
+ 
+    
+            
   $(this).scrollTop(0);
  });
-
 
 
 // Scroll To specified tab
@@ -21,10 +51,61 @@ $('.nav-scroll').on('click',function(event) {
   var target = $(this).attr('href');
   
   $('html, body').animate({
-    scrollTop: $(target).offset().top
+    scrollTop: $(target).offset().top-50
   }, 800);
 });
 
+
+
+
+
+ $("#regForm").validate({
+       errorElement:'p',
+    rules: {
+      name: "required",
+      email: {
+        required: true,
+        email: true,
+      },
+      passwd: {
+        required: true,
+        minlength: 6,
+      },
+      confpasswd: {
+        required: true,
+        minlength: 6,
+        equalTo:'#passwd',
+      },
+      dob:{required:true,
+       dateITA:true,
+      },
+      agree:"required",
+    },
+    messages: {
+      name: "Please enter your name",
+      passwd: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 6 characters long"
+      },
+      confpasswd: {
+        required: "Please provide a password confirmation",
+        minlength: "Your password must be at least 6 characters long",
+        equalTo:"Your passwords do not match"
+      },
+      dob:{required: "Please enter your  date of birth",
+  dateITA:"Please enter date in format DD/MM/YYYY",
+      },
+      email: {email:"Please enter a valid email address",
+      required:"Please enter your email address",},
+      agree:"Please accept our terms and conditions",
+    },
+    
+    submitHandler: function(form) {
+      
+$(".btn-custom").val("THANK YOU!");
+      form.submit();
+    }
+  });
 
 
 
